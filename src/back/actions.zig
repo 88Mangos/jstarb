931bac389747ef1bbad3589e804a53ccb5d2f848
Tyler Yang <tjyang2005@gmail.com>
2026-05-09 21:32:52 -0500

    actions: Manager pattern

--- src/back/actions.zig
+++ src/back/actions.zig
@@ -6,50 +6,63 @@ const data = @import("data.zig");
 
 pub const EntryUpdateError = error{InvalidUpdate};
 
-//
-// MARK: Add Entries to DB
-//
+pub const Manager = struct {
+    allocator: std.mem.Allocator,
+    entries: std.ArrayList(data.Entry),
 
-pub fn add_job_opening() data.Entry {}
-pub fn add_outreach_event() data.Entry {}
-pub fn add_coffee_chat() data.Entry {}
-pub fn add_resume_bucket() data.Entry {}
+    pub fn deinit(self: *Manager) void {
+        for (self.entries.items) |*entry| {
+            entry.deinit(self.allocator); // Free strings and ledger
+        }
+        self.entries.deinit();
+    }
 
-//
-// MARK: Updates
-// NOTE: there are no workflows for updating resume buckets,
-//  since you just submit a resume and that's that.
-//
+    //
+    // MARK: Add Entries to DB
+    //
 
-//
-// MARK: Update Job Openings
-//
+    pub fn add_job_opening() data.Entry {}
+    pub fn add_outreach_event() data.Entry {}
+    pub fn add_coffee_chat() data.Entry {}
+    pub fn add_resume_bucket() data.Entry {}
 
-pub fn update_job_opening() EntryUpdateError!void {}
+    //
+    // MARK: Updates
+    // NOTE: there are no workflows for updating resume buckets,
+    //  since you just submit a resume and that's that.
+    //
 
-// if it's been too long since last new new ledger entry...
+    //
+    // MARK: Update Job Openings
+    //
 
-// and we haven't submitted an application to the job_opening,
-//  if there's a due date and it's passed,
-//    Entry.job_opening.state.pending = DeadlinePassed
-//  else,
-//    Entry.job_opening.state.pending = Ignored
-// and we haven't heard back after submitting (including if we've interviewed)
-//  Entry.job_opening.state.noOffer = Ghosted
+    pub fn update_job_opening() EntryUpdateError!void {}
 
-//
-// MARK: Update Outreach Events
-//
-pub fn update_outreach_event() EntryUpdateError!void {}
+    // if it's been too long since last new new ledger entry...
 
-//
-// MARK: Update Coffee Chats
-//
-pub fn update_coffee_chat() EntryUpdateError!void {}
+    // and we haven't submitted an application to the job_opening,
+    //  if there's a due date and it's passed,
+    //    Entry.job_opening.state.pending = DeadlinePassed
+    //  else,
+    //    Entry.job_opening.state.pending = Ignored
+    // and we haven't heard back after submitting (including if we've interviewed)
+    //  Entry.job_opening.state.noOffer = Ghosted
 
-//
-// MARK: Query DB Entries for Charts
-//
+    //
+    // MARK: Update Outreach Events
+    //
+    pub fn update_outreach_event() EntryUpdateError!void {}
+
+    //
+    // MARK: Update Coffee Chats
+    //
+    pub fn update_coffee_chat() EntryUpdateError!void {}
+
+    //
+    // MARK: Query DB Entries for Charts
+    //
+
+};
 
 //
 // MARK: Tests for Actions
