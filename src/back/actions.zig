922bce862b934afaa396a0438def61fc6b8b46cb
Tyler Yang <tjyang2005@gmail.com>
2026-05-09 21:47:19 -0500

    actions: Entry-specific namespaces

--- src/back/actions.zig
+++ src/back/actions.zig
@@ -16,30 +16,39 @@ pub const Manager = struct {
         }
         self.entries.deinit();
     }
+};
+
+//
+// MARK: Namespaces for Entry Updates
+// NOTE: we don't really need a namespace for resume buckets,
+//  since you just submit a resume and that's that.
+//
+
+//
+// MARK: Job Opening Namespace
+//
+pub const job_openings = struct {
+    pub fn new(mgr: *Manager) !*data.Entry {
+        var entry = data.Entry{};
+    }
+
+    pub fn apply(mgr: *Manager) !*data.Entry {}
+    pub fn receive_oa(mgr: *Manager) !*data.Entry {}
+    pub fn complete_oa(mgr: *Manager) !*data.Entry {}
+    pub fn receive_interview(mgr: *Manager) !*data.Entry {}
+    pub fn schedule_interview(mgr: *Manager) !*data.Entry {}
+    pub fn reschedule_interview(mgr: *Manager) !*data.Entry {}
+    pub fn complete_interview(mgr: *Manager) !*data.Entry {}
+    pub fn receive_offer(mgr: *Manager) !*data.Entry {}
+    pub fn accept_offer(mgr: *Manager) !*data.Entry {}
+    pub fn reject_offer(mgr: *Manager) !*data.Entry {}
+    pub fn receive_rejection(mgr: *Manager) !*data.Entry {}
+    pub fn withdraw_application(mgr: *Manager) !*data.Entry {}
 
     //
-    // MARK: Add Entries to DB
-    //
-
-    pub fn add_job_opening() data.Entry {}
-    pub fn add_outreach_event() data.Entry {}
-    pub fn add_coffee_chat() data.Entry {}
-    pub fn add_resume_bucket() data.Entry {}
-
-    //
-    // MARK: Updates
-    // NOTE: there are no workflows for updating resume buckets,
-    //  since you just submit a resume and that's that.
-    //
-
-    //
-    // MARK: Update Job Openings
-    //
-
-    pub fn update_job_opening() EntryUpdateError!void {}
-
+    // Functions that run automatically
     // if it's been too long since last new new ledger entry...
-
+    //
     // and we haven't submitted an application to the job_opening,
     //  if there's a due date and it's passed,
     //    Entry.job_opening.state.pending = DeadlinePassed
@@ -47,22 +56,38 @@ pub const Manager = struct {
     //    Entry.job_opening.state.pending = Ignored
     // and we haven't heard back after submitting (including if we've interviewed)
     //  Entry.job_opening.state.noOffer = Ghosted
-
-    //
-    // MARK: Update Outreach Events
-    //
-    pub fn update_outreach_event() EntryUpdateError!void {}
-
-    //
-    // MARK: Update Coffee Chats
-    //
-    pub fn update_coffee_chat() EntryUpdateError!void {}
-
-    //
-    // MARK: Query DB Entries for Charts
-    //
-
 };
+
+//
+// MARK: Outreach Event Namespace
+//
+pub const outreach_event = struct {
+    pub fn new(mgr: *Manager) !*data.Entry {
+        var entry = data.Entry{};
+    }
+
+    pub fn apply(mgr: *Manager) !*data.Entry {}
+    pub fn receive_oa(mgr: *Manager) !*data.Entry {}
+    pub fn complete_oa(mgr: *Manager) !*data.Entry {}
+    pub fn attend_event(mgr: *Manager) !*data.Entry {}
+    pub fn receive_rejection(mgr: *Manager) !*data.Entry {}
+};
+
+//
+// MARK: Coffee Chat Namespace
+//
+pub const coffee_chats = struct {
+    pub fn new(mgr: *Manager) !*data.Entry {
+        var entry = data.Entry{};
+    }
+    pub fn schedule(mgr: *Manager) !*data.Entry {}
+    pub fn complete(mgr: *Manager) !*data.Entry {}
+};
+
+//
+// MARK: Resume Bucket logic
+//
+pub fn resume_bucket_new(mgr: *Manager) !*data.Entry {}
 
 //
 // MARK: Tests for Actions
