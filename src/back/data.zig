6f440ef35201fc0fc15399be1862d0b9d12f12f3
Tyler Yang <tjyang2005@gmail.com>
2026-05-11 14:34:31 -0500

    scaffold Update struct

--- src/back/data.zig
+++ src/back/data.zig
@@ -49,15 +49,45 @@ pub const Entry = struct {
 
     ledger: std.MultiArrayList(Update),
 
-    pub fn init() Entry {}
     pub fn deinit() !void {}
 };
 
+//
+// The actions taken by a company are in the past tense,
+//  e.g., "received" an OA/interview/offer,
+//  or (god forbid) "rescinded" an offer.
+//
+// The actions taken by the user are in the present tense,
+//  and by default, actions are from the user,
+//  e.g., rescheduling an interview is usually because of the interviewer (company),
+//  but the user is responsible for doing that reschedule.
+//
 pub const Update = struct {
     id: u64,
     created_at: i64,
+    verb: enum {
+        // Company Actions
+        Received,
+        Ghosted,
+        Offered,
+        Rejected,
+        Rescinded,
+
+        // User Actions
+        Schedule,
+        Reschedule,
+        Complete,
+        Accept,
+        Reject,
+        Reneg,
+        Submit,
+        Ignore,
+
+        // Manual updates
+        UpdateEntry,
+    },
+    item: ?enum { OA, Interview, Offer, Resume },
     // using reflection to figure out the function call that created this event
 
-    pub fn init() Update {}
     pub fn deinit() !void {}
 };
